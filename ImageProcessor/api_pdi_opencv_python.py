42d0703f54b1c7ea8f87dcffc15659c9b547e15f
CristianoSouza <cristianoantonio.souza10@gmail.com>
2015-08-01 00:26:58 -0300

    ha potennnnça

--- ImageProcessor/api_pdi_opencv_python.py
+++ ImageProcessor/api_pdi_opencv_python.py
@@ -3,7 +3,8 @@
 
 import numpy as np
 import cv2
-from image import Image 
+from image import Image
+from math import sqrt
 
 class ImageProcessor:
 
@@ -29,7 +30,60 @@ class ImageProcessor:
 	cv2.destroyAllWindows()
 
     @staticmethod
+    def color_extraction(color, image):
+        color= [0,0,0]
+        print color
+        _row = image.image.shape[0]
+        _column = image.image.shape[1]
+        print _row
+        print _column
+        b,g,r = cv2.split(image.image)
+        print image.image[1][1]
+
+        for x in xrange(1,_row):
+            for y in xrange(1,_column):
+                _distance = sqrt((pow((b[x][y] - color[1]),2))+(pow((g[x][y] - color[0]),2))+(pow((r[x][y] - color[2]),2)))
+                if (_distance < 100):
+                    image.image[x][y] = 255 
+        
+        cv2.imshow("Image2", image.image)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+    @staticmethod
     def rbg_to_cinza(image):
 	gray_image = cv2.cvtColor( image.image, cv2.COLOR_BGR2GRAY);
 	image.image = gray_image
 	return image
+
+    @staticmethod
+    def crop(image, xi, xf, yi, yf):
+        image_crop = image.image[xi:xf, yi:yf]
+        return image_crop
+    
+    @staticmethod
+    def binarization(image, value):
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        ret,binary_image = cv2.threshold(gray_image, value,255,cv2.THRESH_BINARY)
+        return binary_image
+
+    '''@staticmethod
+    def resize(image):
+        r = 100.0 / image.image.shape[1]
+        dim = (500, int(image.image.shape[0] * r * 3 ))
+        resized_image = cv2.resize(image.image, dim, interpolation = cv2.INTER_NEAREST )
+        return resized_image'''
+
+    @staticmethod
+    def low_pass( image ):
+        blur = cv2.blur( image.image, (3,3))
+        return blur
+    
+    '''@staticmethod
+    def generate_histogram(image):
+        hist = cv2.calcHist([image],[0],None,[256],[0,256])
+        plt.subplot(221)
+        plt.plot(hist)
+        plt.show()'''
+
+    
--- ImageProcessor/teste.py
+++ ImageProcessor/teste.py
@@ -2,11 +2,23 @@
 #-*- coding: utf-8 -*-
 
 from api_pdi_opencv_python import ImageProcessor
+import numpy as np
+import cv2
+from image import Image
+from math import sqrt
 
-image = ImageProcessor()
-object_image = image.read_image("../tucanu.jpg")
-image.show_image(object_image)
-object_image = image.rbg_to_cinza(object_image)
-image.show_image(object_image)
-image.save_image("tucanu_gray.jpg", object_image)
-
+image_processor = ImageProcessor()
+object_image = image_processor.read_image("blur.jpg")
+#image_processor.show_image(object_image)
+#object_image = image_processor.rbg_to_cinza(object_image)
+#image_processor.show_image(object_image)
+#image_processor.save_image("tucanu_gray.jpg", object_image)
+#image.generate_histogram(object_image)
+#aa = image_processor.color_extraction("blue", object_image)
+#image = image_processor.crop(object_image, 100, 400, 100, 600)
+##aa = image_processor.binarization(image, 120)
+#aa = image_processor.resize(object_image)
+aa = image_processor.low_pass(object_image)
+cv2.imshow("Imageasd2", aa )
+cv2.waitKey(0)
+cv2.destroyAllWindows()
